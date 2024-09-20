86199fddb961be633e69546954efb75c5102fc48
lsc72 <lsc72sio@gmail.com>
2024-09-20 10:25:10 +0200

    Update media_10.1.3.4_page.js

--- RS1/course/module5/5.1.3.4/media/media_10.1.3.4_page.js
+++ RS1/course/module5/5.1.3.4/media/media_10.1.3.4_page.js
@@ -5,7 +5,7 @@ loadScript("../../../common/scripts/swfobject.js", registerSWF);
 
 (Animation = function(control,id){
 var manifest = [
-		{src:"images/_10_1_3_3.png", id:"_10_1_3_3"},
+		{src:"images/10_1_3_3.png", id:"_10_1_3_3"},
 		{src:"images/envelope_closed_static.png", id:"envelope_closed_static"},
 		{src:"images/Switch_workgroup.png", id:"Switch_workgroup"}
 	];
