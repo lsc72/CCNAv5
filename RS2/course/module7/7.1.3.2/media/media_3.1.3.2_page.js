6185e1a2eed84ecfc04be9b342bbdf15b853b185
lsc72 <lsc72sio@gmail.com>
2026-09-16 09:45:01 +0200

    Update media_3.1.3.2_page.js

--- RS2/course/module7/7.1.3.2/media/media_3.1.3.2_page.js
+++ RS2/course/module7/7.1.3.2/media/media_3.1.3.2_page.js
@@ -6,7 +6,7 @@ var mainMovieClip;
 
 (Animation = function(control,id){	
 		var manifest = [
-		{src:"images/_3132.jpg", id:"_3132"},
+		{src:"images/3132.jpg", id:"_3132"},
 		{src:"images/Router.png", id:"Router"},
 		{src:"images/Wan_line.png", id:"Wan_line"}
 	];
